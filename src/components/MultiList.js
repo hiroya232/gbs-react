958ddc267d806b2bd70d6d4e27e1cd0e1fabedbc
hiroya-akeoka <mowoarm232@gmail.com>
2021-07-11 12:33:30 +0900

    一時コミット

--- src/components/MultiList.js
+++ src/components/MultiList.js
@@ -2,6 +2,14 @@ import React from 'react'
 import Twitter from 'twitter'
 
 class MultiList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.getTweet = this.getTweet.bind(this)
+    this.state = {
+      multiList: [],
+    }
+  }
+
   getTweet() {
     let client = new Twitter({
       consumer_key: process.env.REACT_APP_CONSUMER_KEY,
@@ -11,6 +19,7 @@ class MultiList extends React.Component {
     })
 
     //ツイート取得
+    let self = this
     client.stream('statuses/filter', { track: ':参戦ID' }, function (stream) {
       stream.on('data', function (tweet) {
         let splittedTweet = tweet.text.split(/[ \n]/)
@@ -26,6 +35,13 @@ class MultiList extends React.Component {
           multiInfo: splittedTweet[i + 2] + ' ' + splittedTweet[i + 3] + ' ' + splittedTweet[i - 1],
         }
         console.log('multiInfos : ', multiInfos.multiInfo)
+
+        const multiList_copy = self.state.multiList.slice()
+        multiList_copy.push(multiInfos)
+        this.setState({ multiList: multiList_copy })
+        // if (self.multiList.length > 10) {
+        //   self.multiList.shift()
+        // }
       })
 
       stream.on('error', function (error) {
@@ -35,9 +51,14 @@ class MultiList extends React.Component {
   }
 
   render() {
+    const multiList = this.state.multiList.map((multiList, index) => (
+      // Only do this if items have no stable IDs
+      <li key={index}>{multiList}</li>
+    ))
     return (
       <div>
         <button onClick={this.getTweet}>ツイート取得</button>
+        <ul>{multiList}</ul>
       </div>
     )
   }
